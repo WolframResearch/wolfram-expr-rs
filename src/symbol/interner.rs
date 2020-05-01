c9b25f561530bf9cf9ca68bea3c6ff894709442e
Connor Gray <connorg@wolfram.com>
2020-05-01 11:37:04 -0500

    Remove InternedString::unsafe_internal_data()
    This leaks implementation details and isn't necessary.

--- src/symbol/interner.rs
+++ src/symbol/interner.rs
@@ -76,17 +76,3 @@ impl<S: Into<String> + AsRef<str>> From<S> for InternedString {
         InternedString(value)
     }
 }
-
-impl InternedString {
-    pub unsafe fn unsafe_internal_data(&self) -> usize {
-        self.0
-    }
-}
-
-// impl<'a> From<&'a str> for InternedString {
-//     fn from(s: &'a str) -> InternedString {
-//         let mut lock = acquire_lock();
-//         let value: usize = lock.get_or_intern(s);
-//         InternedString(value)
-//     }
-// }
